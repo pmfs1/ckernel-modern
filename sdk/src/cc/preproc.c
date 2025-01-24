4c0485f892cfb6f7297225c286c5eecec091ff6a
Pedro S. <pedromiguelfilipesimoes@proton.me>
2025-01-24 15:35:25 +0000

    Revert "fix cpp/use-of-goto in sdk/src/cc/preproc.c (void preprocess_skip(void))"
    This reverts commit b4088f52c7814114330cbd285e234d40eae7fb5f.

--- sdk/src/cc/preproc.c
+++ sdk/src/cc/preproc.c
@@ -708,21 +708,18 @@ uint8_t *parse_pp_string(uint8_t *p, int sep, CString *str)
 // Skip block of text until #else, #elif or #endif. skip also pairs of #if/#endif
 void preprocess_skip(void)
 {
-    int a, c;
-    int start_of_line = 1;
-    int in_warn_or_error = 0;
-    uint8_t *p = file->buf_ptr;
-    a = 0;
+    int a, start_of_line, c, in_warn_or_error;
+    uint8_t *p;
 
+    p = file->buf_ptr;
+    a = 0;
     while (1)
     {
         start_of_line = 1;
         in_warn_or_error = 0;
-
         while (1)
         {
             c = *p;
-
             switch (c)
             {
             case ' ':
@@ -732,13 +729,11 @@ void preprocess_skip(void)
             case '\r':
                 p++;
                 continue;
-
             case '\n':
                 file->line_num++;
                 p++;
                 start_of_line = 1;
                 continue;
-
             case '\\':
                 file->buf_ptr = p;
                 c = handle_eob();
@@ -753,41 +748,30 @@ void preprocess_skip(void)
                 }
                 p = file->buf_ptr;
                 continue;
-
+                // Skip strings
             case '\"':
             case '\'':
-                if (!in_warn_or_error)
-                {
-                    p = parse_pp_string(p, c, NULL);
-                }
-                else
-                {
-                    p++;
-                }
+                if (in_warn_or_error)
+                    break;
+                p = parse_pp_string(p, c, NULL);
                 break;
-
+                // Skip comments
             case '/':
-                if (!in_warn_or_error)
+                if (in_warn_or_error)
+                    break;
+                file->buf_ptr = p;
+                ch = *p;
+                minp();
+                p = file->buf_ptr;
+                if (ch == '*')
                 {
-                    file->buf_ptr = p;
-                    ch = *p;
-                    minp();
-                    p = file->buf_ptr;
-                    if (ch == '*')
-                    {
-                        p = parse_comment(p);
-                    }
-                    else if (ch == '/')
-                    {
-                        p = parse_line_comment(p);
-                    }
+                    p = parse_comment(p);
                 }
-                else
+                else if (ch == '/')
                 {
-                    p++;
+                    p = parse_line_comment(p);
                 }
                 break;
-
             case '#':
                 p++;
                 if (start_of_line)
@@ -795,13 +779,8 @@ void preprocess_skip(void)
                     file->buf_ptr = p;
                     next_nomacro();
                     p = file->buf_ptr;
-                    
                     if (a == 0 && (tok == TOK_ELSE || tok == TOK_ELIF || tok == TOK_ENDIF))
-                    {
-                        file->buf_ptr = p;
-                        return;
-                    }
-                    
+                        goto done;
                     if (tok == TOK_IF || tok == TOK_IFDEF || tok == TOK_IFNDEF)
                     {
                         a++;
@@ -816,15 +795,16 @@ void preprocess_skip(void)
                     }
                 }
                 break;
-
             default:
                 p++;
                 break;
             }
-
             start_of_line = 0;
         }
     }
+
+done:
+    file->buf_ptr = p;
 }
 
 // ParseState handling
