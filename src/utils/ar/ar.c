c4d2a052406c5ceb549a0fa1964c54bde5e6a170
Pedro S. <pedromiguelfilipesimoes@proton.me>
2025-01-27 08:30:30 +0000

    Fixed unnecessary NULL check before call to free (cpp/guarded-free) in src/utils/ar/ar.c:216 and src/utils/ar/ar.c:226.
    Signed-off-by: GitHub <noreply@github.com>

--- src/utils/ar/ar.c
+++ src/utils/ar/ar.c
@@ -213,8 +213,7 @@ void free_archive(struct archive *ar)
     while (e)
     {
         struct entry *next = e->next;
-        if (e->contents)
-            free(e->contents);
+        free(e->contents);
         free(e);
         e = next;
     }
@@ -223,8 +222,7 @@ void free_archive(struct archive *ar)
     while (s)
     {
         struct symbol *next = s->next;
-        if (s->name)
-            free(s->name);
+        free(s->name);
         free(s);
         s = next;
     }
