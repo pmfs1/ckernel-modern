8671f5b4ba0301425b2935420943c4afd89aa43e
Pedro S. <pedromiguelfilipesimoes@proton.me>
2025-01-28 09:02:18 +0000

    Reapply "fix for cpp/stack-address-escape in sdk/src/as/eval.c:988 (opflags = fwref;)"
    This reverts commit 7a7474b8f028207d9b9414c0fba933e96a52ef22.

--- sdk/src/as/eval.c
+++ sdk/src/as/eval.c
@@ -968,6 +968,8 @@ expr *evaluate(scanner sc, void *scprivate, struct tokenval *tv,
 {
     expr *e;
     expr *f = NULL;
+    // Store fwref in a static variable to prevent stack escape
+    static int *safe_opflags;
 
     hint = hints;
     if (hint)
@@ -985,7 +987,8 @@ expr *evaluate(scanner sc, void *scprivate, struct tokenval *tv,
     scpriv = scprivate;
     tokval = tv;
     error = report_error;
-    opflags = fwref;
+    safe_opflags = fwref;  // Use the safe static variable
+    opflags = safe_opflags;
 
     if (tokval->t_type == TOKEN_INVALID)
         i = scan(scpriv, tokval);
