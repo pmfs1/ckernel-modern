aa3381dcfb16d8c0d2b44426e0af8cef53eb7060
Pedro S. <pedromiguelfilipesimoes@proton.me>
2025-01-28 09:10:07 +0000

    fix for cpp/stack-address-escape in sdk/src/as/eval.c:972 (hint = hints;).
    Signed-off-by: GitHub <noreply@github.com>

--- sdk/src/as/eval.c
+++ sdk/src/as/eval.c
@@ -972,11 +972,15 @@ expr *evaluate(scanner sc, void *scprivate, struct tokenval *tv,
 {
     expr *e;
     expr *f = NULL;
+
     static int *safe_opflags;
     static struct tokenval safe_tokval;
     static void *static_scpriv;
+    static struct eval_hints static_hints;
 
-    hint = hints;
+    static_hints = *hints;
+    hint = &static_hints;
+
     if (hint)
         hint->type = EAH_NOHINT;
 
